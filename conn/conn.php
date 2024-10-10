3fc73ef15791b0659fed04ca09cee1fb86aa14ef
shemdg <shemrdg@gmail.com>
2024-10-11 00:45:33 +0800

    message

---
+++ conn/conn.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "registration_login_db";
+$port = "3308";
+
+try {
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$db", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Failed " . $e->getMessage();
+}
+
+?>
