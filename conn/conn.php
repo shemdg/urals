5cfbae5168f9b533e63e289631c5e493bfd0caa8
shemdg <106575164+shemdg@users.noreply.github.com>
2024-10-11 15:52:03 +0800

    Update conn.php

--- conn/conn.php
+++ conn/conn.php
@@ -7,7 +7,7 @@ $db = "registration_login_db";
 $port = "3308";
 
 try {
-    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$db", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Failed " . $e->getMessage();
